83e4ba59accfd913db2d1ae34071d5f11e911bd0
jnxyx <821969066@qq.com>
2016-08-30 09:49:51 +0800

    add range of plugin env

--- src/myUpload.js
+++ src/myUpload.js
@@ -23,205 +23,208 @@
  * 加载功能  --- 待开发
  * 
  */
-function myUpload(options) {
-    if (!(this instanceof myUpload)) {
-        return new myUpload(options);
+;
+(function() {
+
+    var global = this;
+
+    function myUpload(options) {
+        if (!(this instanceof myUpload)) {
+            return new myUpload(options);
+        }
+        this.init(options);
+
+        return this;
     }
-    this.init(options);
 
-    return this;
-}
-
-myUpload.prototype = {
-    init: function(options) {
-        var self = this;
-        if (!self.validateOptions(options)) {
-            throw '参数错误';
-        }
-
-        self.xhr = new XMLHttpRequest();
-        self.formData = new FormData();
-        self.element = options.element;
-        self.url = options.url;
-        self.options = options;
-
-        self.index = 0;
-        self.indexContainer = [];
-
-        //初始化ajax事件
-        self.initAjaxEvent();
-
-        //初始化元素事件
-        self.initEvent();
-
-        // self.sendFile();
-
-    },
-
-    initAjaxEvent: function() {
-        var self = this;
-        var xhr = self.xhr;
-
-        // self.xhr.setRequestHeader("Content-type", "application/json");
-
-        self.xhr.upload.addEventListener("progress", function(e) {
-            self.progress.call(self, e);
-        }, false);
-
-        self.addEvent(xhr, "load", function(e) {
-            self.success.call(self, e);
-        });
-
-        self.addEvent(xhr, "error", function(e) {
-            self.error.call(self, e);
-        });
-
-        self.addEvent(xhr, "readystatechange", function(e) {
-            // console.log(self.xhr.readyState);
-            // console.log(self.xhr.status);
-        });
-    },
-
-    initEvent: function() {
-        var self = this;
-        if (self.options.dragElement) {
-
-            var dragElement = self.options.dragElement;
-            var dragClass = self.options.dragClass || '';
-
-            self.addEvent(dragElement, 'dragover', function(e) {
-                //阻止浏览器默认事件
-                e.preventDefault();
-                //阻止浏览器默认事件扩散
-                e.stopPropagation();
-                if (dragClass) {
-                    dragElement.classList.add(dragClass);
-                }
-            });
-            self.addEvent(dragElement, 'dragleave', function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-                if (dragClass) {
-                    dragElement.classList.remove(dragClass);
-                }
-            });
-            self.addEvent(dragElement, 'drop', function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-                self.sendFile(e.dataTransfer.files[0]);
-            });
-        }
-
-        if (self.options.element) {
-
-            var element = self.options.element;
-
-            self.addEvent(element, 'change', function(e) {
-                self.sendFile();
-            });
-        }
-    },
-
-    //向服务器发送文件
-    sendFile: function(file) {
-        var self = this;
-        var filename = 'files_';
-
-        if (file) {
-            var filesArray = [file];
-        } else {
-            var filesArray = this.element.files;
-        }
-
-        var formData = self.formData;
-
-        //chrome支持，firefox不支持
-        if (formData.forEach) {
-            formData.forEach(function(file, key) {
-                if (formData.has(key)) {
-                    formData.delete(key);
-                }
-            });
-        } else {
-            for (var j in self.indexContainer) {
-                var item = self.indexContainer[j];
-                var key = filename + item;
-                if (formData.has(key)) {
-                    formData.delete(key);
-                }
+    myUpload.prototype = {
+        init: function(options) {
+            var self = this;
+            if (!self.validateOptions(options)) {
+                throw '参数错误';
             }
+
+            self.xhr = new XMLHttpRequest();
+            self.formData = new FormData();
+            self.element = options.element;
+            self.url = options.url;
+            self.options = options;
+
+            self.index = 0;
             self.indexContainer = [];
-        }
 
-        for (var i = 0; i < filesArray.length; i++) {
-            self.indexContainer.push(self.index);
-            formData.append(filename + self.index, filesArray[0]);
-            self.index++;
-        }
+            //初始化ajax事件
+            self.initAjaxEvent();
 
-        this.send();
-    },
+            //初始化元素事件
+            self.initEvent();
 
-    validateOptions: function(options) {
-        if (!options.element || !options.url) {
-            return false;
-        } else {
-            return true;
-        }
-    },
+            // self.sendFile();
 
-    send: function() {
-        this.xhr.open("POST", this.url);
-        this.xhr.send(this.formData);
-    },
+        },
 
-    addEvent: function(el, type, callback) {
-        el.addEventListener(type, callback, false);
-    },
+        initAjaxEvent: function() {
+            var self = this;
+            var xhr = self.xhr;
 
-    success: function(e) {
-        var response = e.target.response;
-        var results = JSON.parse(response);
-        if (this.options.success) {
-            this.options.success(results);
-        } else if (this.options.uploadContainer) {
-            var uploadContainer = this.options.uploadContainer;
-            if (results.data.length) {
-                for (var i = 0; i < results.data.length; i++) {
-                    var item = results.data[i];
-                    if (this.options.uploadTemp) {
-                        var html = this.options.uploadTemp.replace(/(\{src\})/, item);
-                        uploadContainer.innerHTML += html;
+            // self.xhr.setRequestHeader("Content-type", "application/json");
+
+            self.xhr.upload.addEventListener("progress", function(e) {
+                self.progress.call(self, e);
+            }, false);
+
+            self.addEvent(xhr, "load", function(e) {
+                self.success.call(self, e);
+            });
+
+            self.addEvent(xhr, "error", function(e) {
+                self.error.call(self, e);
+            });
+
+            self.addEvent(xhr, "readystatechange", function(e) {
+                // console.log(self.xhr.readyState);
+                // console.log(self.xhr.status);
+            });
+        },
+
+        initEvent: function() {
+            var self = this;
+            if (self.options.dragElement) {
+
+                var dragElement = self.options.dragElement;
+                var dragClass = self.options.dragClass || '';
+
+                self.addEvent(dragElement, 'dragover', function(e) {
+                    //阻止浏览器默认事件
+                    e.preventDefault();
+                    //阻止浏览器默认事件扩散
+                    e.stopPropagation();
+                    if (dragClass) {
+                        dragElement.classList.add(dragClass);
+                    }
+                });
+                self.addEvent(dragElement, 'dragleave', function(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    if (dragClass) {
+                        dragElement.classList.remove(dragClass);
+                    }
+                });
+                self.addEvent(dragElement, 'drop', function(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    self.sendFile(e.dataTransfer.files[0]);
+                });
+            }
+
+            if (self.options.element) {
+
+                var element = self.options.element;
+
+                self.addEvent(element, 'change', function(e) {
+                    self.sendFile();
+                });
+            }
+        },
+
+        //向服务器发送文件
+        sendFile: function(file) {
+            var self = this;
+            var filename = 'files_';
+
+            if (file) {
+                var filesArray = [file];
+            } else {
+                var filesArray = this.element.files;
+            }
+
+            var formData = self.formData;
+
+            //chrome支持，firefox不支持
+            if (formData.forEach) {
+                formData.forEach(function(file, key) {
+                    if (formData.has(key)) {
+                        formData.delete(key);
+                    }
+                });
+            } else {
+                for (var j in self.indexContainer) {
+                    var item = self.indexContainer[j];
+                    var key = filename + item;
+                    if (formData.has(key)) {
+                        formData.delete(key);
+                    }
+                }
+                self.indexContainer = [];
+            }
+
+            for (var i = 0; i < filesArray.length; i++) {
+                self.indexContainer.push(self.index);
+                formData.append(filename + self.index, filesArray[0]);
+                self.index++;
+            }
+
+            this.send();
+        },
+
+        validateOptions: function(options) {
+            if (!options.element || !options.url) {
+                return false;
+            } else {
+                return true;
+            }
+        },
+
+        send: function() {
+            this.xhr.open("POST", this.url);
+            this.xhr.send(this.formData);
+        },
+
+        addEvent: function(el, type, callback) {
+            el.addEventListener(type, callback, false);
+        },
+
+        success: function(e) {
+            var response = e.target.response;
+            var results = JSON.parse(response);
+            if (this.options.success) {
+                this.options.success(results);
+            } else if (this.options.uploadContainer) {
+                var uploadContainer = this.options.uploadContainer;
+                if (results.data.length) {
+                    for (var i = 0; i < results.data.length; i++) {
+                        var item = results.data[i];
+                        if (this.options.uploadTemp) {
+                            var html = this.options.uploadTemp.replace(/(\{src\})/, item);
+                            uploadContainer.innerHTML += html;
+                        }
                     }
                 }
             }
+        },
+
+        error: function(e) {
+            if (this.options.error) {
+                this.options.error(e);
+            }
+        },
+
+        progress: function(e) {
+            var percent = 100 * e.loaded / e.total;
+            percent = percent.toFixed(2);
+            percent += '%';
+            if (this.options.progress) {
+                this.options.progress(percent);
+            }
+        },
+
+        getId: function(id) {
+            return global.document.getElementById(id);
+        },
+
+        getClass: function(className) {
+            return global.document.getElementsByClassName(className);
         }
-    },
 
-    error: function(e) {
-        if (this.options.error) {
-            this.options.error(e);
-        }
-    },
-
-    progress: function(e) {
-        var percent = 100 * e.loaded / e.total;
-        percent = percent.toFixed(2);
-        percent += '%';
-        if (this.options.progress) {
-            this.options.progress(percent);
-        }
-    },
-
-    getId: function(id) {
-        return document.getElementById(id);
-    },
-
-    getClass: function(className) {
-        return document.getElementsByClassName(className);
     }
-
-}
-
-
-
+}).call(this);
