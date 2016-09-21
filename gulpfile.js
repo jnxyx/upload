8c4aff09f7eaab63def428a42d0fe7f8a40738c4
jnxyx <821969066@qq.com>
2016-09-21 18:47:25 +0800

    gulp reset

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,18 @@ var gulp = require('gulp');
 var uglify = require("gulp-uglify");
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var bufferf = require('gulp-buffer');
+var streamify = require('gulp-streamify');
 
 gulp.task('minify-js', function() {
+	var files = source('src/upload.js');
+	console.log(files);
+
+	gulp.src('src/upload.js').on('data',function(file){
+		console.log(file.contents);
+	});
+
 	gulp.src('src/upload.js') 
 		//.pipe(concat('all.js'))
 		.pipe(uglify()) 
